b5d395bb248ebdea4588e72edae62a8ca55d80b5
Ludovic Barman <ludovic.barman@gmail.com>
2018-07-17 21:53:36 +0200

    Fixes

--- feed.py
+++ feed.py
@@ -13,10 +13,28 @@ def rotate(angle):
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_TO_USE, GPIO.OUT)
+GPIO.setwarnings(False)
+
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+
+def my_callback(channel):  
+        print "falling edge detected on 17" 
+
+GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback, bouncetime=300)
+
 pwm = GPIO.PWM(GPIO_TO_USE, carrierWidth)
 
-pwm.start(getDuty(position1))
-rotate(position1)
-rotate(position2)
-rotate(position1)
+pwm.start(getDuty(position1Degree))
+rotate(position1Degree)
+rotate(position2Degree)
+rotate(position1Degree)
 time.sleep(1)
+
+
+try:  
+    while True:
+        print "Sleeping"
+        time.sleep(1)
+except KeyboardInterrupt:  
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+GPIO.cleanup()
