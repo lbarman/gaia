624611ebfc280c476f843157ac3b4b8b969eb0c0
Ludovic Barman <lbarman@protonmail.ch>
2018-07-30 14:33:31 +0200

    New client, can write commands, live feed

--- server/index.php
+++ server/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once("db.php");
+require_once("config.php");
 
 $NUMBER_OF_ITEMS_TO_FETCH = 10000;
 
@@ -14,9 +15,9 @@ function startsWith($haystack, $needle)
      return (substr($haystack, 0, $length) === $needle);
 }
 
-function handleRaspberryConnection($db){
+function handleRaspberryConnection($db, $nextCommand){
     if(isset($_GET['ping'])) {
-        $secretToken = 'nSWXz8p7BsdY74NGNQVqJr4e';
+        $secretToken = CLIENT_TOKEN;
         $secretToken2 = '';
 
         if(isset($_GET['token'])){
@@ -38,18 +39,59 @@ function handleRaspberryConnection($db){
             $s = "INSERT INTO `pings` (`id`, `timestamp`, `local_timestamp`, `data`) VALUES (NULL, CURRENT_TIMESTAMP, '" . $timestamp ."', '". $data ."');";
             $db->query($s);
 
-            die("OK");
+            // truncate the DB
+            $oneWeekAgo = date("Y-m-d H:i:s", strtotime("-1 week"));
+            $s = "DELETE FROM `pings` WHERE `timestamp`<'" . $oneWeekAgo ."';";
+            $db->query($s);
+
+            // empty commands
+            $s = "DELETE FROM `commands` WHERE 1=1;";
+            $db->query($s);
+
+            die($nextCommand);
         }
         else
         {
-            sleep(1);
+            sleep(3);
             die('Invalid secret');
         }
     }
 }
 
-$db = new Db();                 # connect to DB
-handleRaspberryConnection($db); # check if connection is a ping from the Raspberry
+function testPort($port){
+
+    $red='rgb(255, 99, 132)';
+    $green='rgb(75, 192, 192)';
+
+    if(@fsockopen('lbarman.ch', $port, $errno, $errstr, 2))
+    {
+        return '<span style="color:'.$green.';font-weight:bold;">OK</span>';
+    }
+    return '<span style="color:'.$red.';font-weight:bold;">?</span>';
+}
+
+$db = new Db();
+
+// handle the eventual command creation
+if(isset($_POST) && isset($_POST['passphrase']) && isset($_POST['newCommand'])){
+    if($_POST['passphrase'] !== COMMAND_PASSPHRASE){
+        sleep(3);
+        die("Invalid passphrase");
+    } else {
+        $cmd = strtoupper(trim($_POST['newCommand']));
+        $s = "INSERT INTO `commands` (`id`, `timestamp`, `command`) VALUES (NULL, CURRENT_TIMESTAMP, '" . $cmd ."');";
+        $db->query($s);
+    }
+}
+
+// fetch potential command
+$nextCommand = "OK";
+$commands = $db->select("SELECT id,timestamp,command FROM `commands` ORDER BY timestamp DESC LIMIT 1");
+if(count($commands) > 0) {
+    $nextCommand = $commands[0]["command"];
+}
+
+handleRaspberryConnection($db, $nextCommand); # check if connection is a ping from the Raspberry
 
 ?>
 
@@ -90,77 +132,109 @@ handleRaspberryConnection($db); # check if connection is a ping from the Raspber
         .uninteresting_report {
             display: none;
         }
+        iframe {
+            width: 100%;
+            height: 500px;
+            background-color: black;
+        }
+        #logo {
+            float: left;
+            margin-right: 30px;
+        }
+
+        h1 {
+            margin-top: 30px;
+            margin-bottom: 40px;
+        }
+        body {
+            background-color: white;
+        }
     </style>
     </head>
     <body>
+        <img id="logo" src="images/gaia.jpg" width="100" height="97" />
         <h1>Gaia</h1>
         <div>
+            <p>Connectivity: Ping=<?php echo testPort(11734); ?> Video=<?php echo testPort(11735);?></p>
+            <p><a href="#" onclick="loadFeed()">Load live feed</a></p>
+            <div id="live_feed"></div>
+
             <h3>Graph</h3>
             <div id="canvas-holder1" style="width:100%;">
                 <canvas id="chart1"></canvas>
             </div>
 
-        <div>
-            <?php
+            <div>
+                <?php
 
-                $rows = $db->select("SELECT id,timestamp,local_timestamp,data FROM `pings` ORDER BY timestamp DESC LIMIT ".$NUMBER_OF_ITEMS_TO_FETCH);
-                
-                $toInt = function($s) {
-                    return (int)$s;
-                };
+                    $rows = $db->select("SELECT id,timestamp,local_timestamp,data FROM `pings` ORDER BY timestamp DESC LIMIT ".$NUMBER_OF_ITEMS_TO_FETCH);
+                    
+                    $toInt = function($s) {
+                        return (int)$s;
+                    };
 
-                //for each xTick, check if we have something to print 
-                $igorPing = array();
-                $igorFed = array();
-                $plantsWatered = array();
+                    //for each xTick, check if we have something to print 
+                    $igorPing = array();
+                    $igorFed = array();
+                    $plantsWatered = array();
 
-                foreach($rows as $row) {
-                    $str = $row['local_timestamp'];
-                    $ts = (int)strtotime($row['local_timestamp']);
-                        
-                    $data = base64_decode($row['data']);
-                    if($data == "Feeded"){
-                        $igorFed[$ts] = 1;
-                    } else if(startsWith($data, "Starting plant watering")){
-                        $plantsWatered[$ts] = 1;
-                    } else {
-                        $igorPing[$ts] = 1;
+                    foreach($rows as $row) {
+                        $str = $row['local_timestamp'];
+                        $ts = (int)strtotime($row['local_timestamp']);
+                            
+                        $data = base64_decode($row['data']);
+                        if($data == "Feeded"){
+                            $igorFed[$ts] = 1;
+                        } else if(startsWith($data, "Starting plant watering")){
+                            $plantsWatered[$ts] = 1;
+                        } else {
+                            $igorPing[$ts] = 1;
+                        }
                     }
-                }
 
-                #$maxValue = max(array(max($igorPing), max($igorPing), max($igorPing)));
-                #$igorFed = array_map(function($el) { return $el * $maxValue; }, $igorFed);
-                #$plantsWatered = array_map(function($el) { return $el * $maxValue; }, $plantsWatered);
+                    #$maxValue = max(array(max($igorPing), max($igorPing), max($igorPing)));
+                    #$igorFed = array_map(function($el) { return $el * $maxValue; }, $igorFed);
+                    #$plantsWatered = array_map(function($el) { return $el * $maxValue; }, $plantsWatered);
 
-                //print igorFed for js
-                echo '<script>var a1 = {};';
-                foreach($igorFed as $k => $v){
+                    //print igorFed for js
+                    echo '<script>var a1 = {};';
+                    foreach($igorFed as $k => $v){
 
-                    echo 'a1["'.$k.'"] = \''.$v.'\';';
-                }
+                        echo 'a1["'.$k.'"] = \''.$v.'\';';
+                    }
 
-                //print igorPing for js
-                echo 'var a2 = {};'."\n";
-                foreach($igorPing as $k => $v){
+                    //print igorPing for js
+                    echo 'var a2 = {};'."\n";
+                    foreach($igorPing as $k => $v){
 
-                    echo 'a2["'.$k.'"] = \''.$v.'\';';
-                }
+                        echo 'a2["'.$k.'"] = \''.$v.'\';';
+                    }
 
-                //print plantsWatered for js
-                echo 'var a3 = {};';
-                foreach($plantsWatered as $k => $v){
+                    //print plantsWatered for js
+                    echo 'var a3 = {};';
+                    foreach($plantsWatered as $k => $v){
 
-                    echo 'a3["'.$k.'"] = \''.$v.'\';';
-                }
-                echo "\n".'var igorFed = a1;'."\n";
-                echo 'var igorPing = a2;'."\n";
-                echo 'var plantsWatered = a3;'."\n";
-                echo '</script>';
-                ?>
-                <pre id="graphData">
+                        echo 'a3["'.$k.'"] = \''.$v.'\';';
+                    }
+                    echo "\n".'var igorFed = a1;'."\n";
+                    echo 'var igorPing = a2;'."\n";
+                    echo 'var plantsWatered = a3;'."\n";
+                    echo '</script>';
+                    ?>
+                    <pre id="graphData">
 
-                </pre>
+                    </pre>
+            </div>
         </div>
+        <div>
+            <h3>Commands</h3>
+            <pre><?php echo ($nextCommand == "OK" ? '&bot;' : $nextCommand); ?></pre>
+            <form method="post">
+                <p>Add new command:</p>
+                <input id="newCommand"  name="newCommand" placeholder="COMMAND" type="text" />
+                <input id="passphrase"  name="passphrase" placeholder="Passphrase" type="password" />
+                <input type="submit"></input>
+            </form>
         </div>
         <div>
             <h3>Data</h3>
@@ -343,6 +417,11 @@ handleRaspberryConnection($db); # check if connection is a ping from the Raspber
                 }, 
             });
         };
+
+        function loadFeed(){
+            $('#live_feed').html('<iframe src="http://lbarman.ch:11734"></iframe>');
+            return false;
+        }
     </script>
     </body>
 </html>
