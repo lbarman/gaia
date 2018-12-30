96cb5498d82a13227abcb75130a3e7dd63a51943
Ludovic Barman <lbarman@protonmail.ch>
2018-12-30 14:28:16 +0100

    Finally remove the PHP server

--- server-php/config.sample.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("CLIENT_TOKEN", "client_token");
-define("COMMAND_PASSPHRASE", "abc");
-?>
--- server-php/db_config.sample.php
+++
@@ -1,6 +0,0 @@
-<?php
-$DB['HOST'] = 'mysql';
-$DB['USER'] = 'gaia';
-$DB['PWD'] = 'gaia';
-$DB['DB'] = 'gaia';
-?>
