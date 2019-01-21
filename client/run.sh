104bd5a94184e3bb4e1f0b4670a6431ff227980c
Ludovic Barman <lbarman@protonmail.ch>
2019-01-21 16:02:28 +0100

    Client services etc

--- client/run.sh
+++ client/run.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_NAME="gaia_client.py"
-LOG_FILE_GAIA="logs/gaia-client.txt"
-LOG_FILE_SSH_1="logs/tunnel_ssh.txt"
-LOG_FILE_SSH_2="logs/tunnel_video.txt"
+LOG_FILE_GAIA="gaia-client.txt"
 
 # Always run stuff relative to this very folder, even when calling ./xxx/yyy/folder/gaia/client/run.sh
 cd "$(dirname "$0")"
@@ -21,20 +19,4 @@ if [ "$isRunning" -ne 1 ]; then
 	nohup python2 "${SCRIPT_NAME}" 2>&1 >"$LOG_FILE_GAIA" &
 else
 	echo "Script ${SCRIPT_NAME} already running, not starting"
-fi
-
-# Make sure the script is not running already
-ps -ux | grep "[s]sh -N -R"
-isRunning=$(ps -ux | grep "[s]sh -N -R" | wc -l)
-echo "$isRunning"
-
-if [ "$isRunning" -ne 2 ]; then
-    rm -f "$LOG_FILE_SSH_1"
-    rm -f "$LOG_FILE_SSH_2"
-    echo "Starting ssh tunnels..."
-    kill $(pgrep -f "ssh -N -R")
-    nohup ssh -N -R 11734:localhost:22 raspi@lbarman.ch -p 11733 2>&1 >"$LOG_FILE_SSH_1"  &
-    nohup ssh -N -R 11735:localhost:8081 raspi@lbarman.ch -p 11733 2>&1 >"$LOG_FILE_SSH_2"  &
-else
-    echo "Ssh tunnel already started, not starting"
 fi
